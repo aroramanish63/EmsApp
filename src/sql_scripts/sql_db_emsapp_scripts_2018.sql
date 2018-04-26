7d339fcfb63a547b3403a66d5f534dd11c196634
aroramanish63 <aroramanish63@gmail.com>
2018-04-26 23:43:06 +0530

    Database changes commit
    New Table added in sql scrip files.
    
    Co-Authored-By: praveenmca748 <praveenmca748@users.noreply.github.com>

--- src/sql_scripts/sql_db_emsapp_scripts_2018.sql
+++ src/sql_scripts/sql_db_emsapp_scripts_2018.sql
@@ -14,3 +14,50 @@ CREATE TABLE IF NOT EXISTS category (
     `status` CHAR(10) DEFAULT NULL,
     PRIMARY KEY(id)
 );
+
+CREATE TABLE IF NOT EXISTS subcategory (
+    `id` CHAR(36), 
+    `name` VARCHAR(255) DEFAULT NULL, 
+    `date_entered` DATETIME DEFAULT NULL,
+    `date_modified` DATETIME DEFAULT NULL,
+    `created_by` CHAR(36) DEFAULT NULL,
+    `modified_by` CHAR(36) DEFAULT NULL,
+    `deleted` TINYINT(1) DEFAULT '0',
+    `decription` TEXT DEFAULT NULL,
+    `category_id` CHAR(36) DEFAULT NULL,
+    `status` CHAR(10) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS payment (
+    `id` CHAR(36), 
+    `name` VARCHAR(255) DEFAULT NULL, 
+    `date_entered` DATETIME DEFAULT NULL,
+    `date_modified` DATETIME DEFAULT NULL,
+    `created_by` CHAR(36) DEFAULT NULL,
+    `modified_by` CHAR(36) DEFAULT NULL,
+    `deleted` TINYINT(1) DEFAULT '0',
+    `decription` TEXT DEFAULT NULL,
+    `payment_type` tinyint(2) DEFAULT '0' COMMENT '0=>Expense, 1=>Income',
+    `status` CHAR(10) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+    `id` CHAR(36), 
+    `name` VARCHAR(255) DEFAULT NULL, 
+    `date_entered` DATETIME DEFAULT NULL,
+    `date_modified` DATETIME DEFAULT NULL,
+    `created_by` CHAR(36) DEFAULT NULL,
+    `modified_by` CHAR(36) DEFAULT NULL,
+    `deleted` TINYINT(1) DEFAULT '0',
+    `username` VARCHAR(255) DEFAULT NULL,
+    `userhash` VARCHAR(255) DEFAULT NULL,
+    `first_name` VARCHAR(255) DEFAULT NULL,
+    `last_name` VARCHAR(255) DEFAULT NULL,
+    `email_address` VARCHAR(150) DEFAULT NULL,
+    `mobile_no` BIGINT(12),
+    `login_by` TINYINT(5) DEFAULT '0' COMMENT '0=>Email, 1=>Gmail, 2=>Facebook',
+    `status` CHAR(10) DEFAULT NULL,
+    PRIMARY KEY(id)
+);
