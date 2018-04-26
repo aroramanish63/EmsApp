b54401fc7ecbed3a5a40621e80e824756287cff2
aroramanish63 <aroramanish63@gmail.com>
2018-04-27 00:24:55 +0530

    Columns added in table schema
    Some table schema, column name or column comment has been modified.

--- src/sql_scripts/sql_db_emsapp_scripts_2018.sql
+++ src/sql_scripts/sql_db_emsapp_scripts_2018.sql
@@ -2,8 +2,10 @@ CREATE DATABASE IF NOT EXISTS `emsapp`;
 
 USE `emsapp`;
 
+-- 26th April 2018
+
 CREATE TABLE IF NOT EXISTS category (
-    `id` CHAR(36), 
+    `category_id` CHAR(36), 
     `name` VARCHAR(255) DEFAULT NULL, 
     `date_entered` DATETIME DEFAULT NULL,
     `date_modified` DATETIME DEFAULT NULL,
@@ -11,12 +13,12 @@ CREATE TABLE IF NOT EXISTS category (
     `modified_by` CHAR(36) DEFAULT NULL,
     `deleted` TINYINT(1) DEFAULT '0',
     `decription` TEXT DEFAULT NULL,
-    `status` CHAR(10) DEFAULT NULL,
+    `status` CHAR(10) DEFAULT 'Active' COMMENT 'Active, Inactive',
     PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS subcategory (
-    `id` CHAR(36), 
+    `subcategory_id` CHAR(36), 
     `name` VARCHAR(255) DEFAULT NULL, 
     `date_entered` DATETIME DEFAULT NULL,
     `date_modified` DATETIME DEFAULT NULL,
@@ -25,12 +27,12 @@ CREATE TABLE IF NOT EXISTS subcategory (
     `deleted` TINYINT(1) DEFAULT '0',
     `decription` TEXT DEFAULT NULL,
     `category_id` CHAR(36) DEFAULT NULL,
-    `status` CHAR(10) DEFAULT NULL,
+    `status` CHAR(10) DEFAULT 'Active' COMMENT 'Active, Inactive',
     PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS payment (
-    `id` CHAR(36), 
+    `payment_id` CHAR(36), 
     `name` VARCHAR(255) DEFAULT NULL, 
     `date_entered` DATETIME DEFAULT NULL,
     `date_modified` DATETIME DEFAULT NULL,
@@ -39,25 +41,29 @@ CREATE TABLE IF NOT EXISTS payment (
     `deleted` TINYINT(1) DEFAULT '0',
     `decription` TEXT DEFAULT NULL,
     `payment_type` tinyint(2) DEFAULT '0' COMMENT '0=>Expense, 1=>Income',
-    `status` CHAR(10) DEFAULT NULL,
+    `amount` DOUBLE DEFAULT '0',
+    `subcategory_id` CHAR(36) DEFAULT NULL,
+    `status` CHAR(10) DEFAULT 'Active' COMMENT 'Active, Inactive',
     PRIMARY KEY(id)
 );
 
 CREATE TABLE IF NOT EXISTS users (
-    `id` CHAR(36), 
+    `users_id` CHAR(36), 
     `name` VARCHAR(255) DEFAULT NULL, 
     `date_entered` DATETIME DEFAULT NULL,
     `date_modified` DATETIME DEFAULT NULL,
     `created_by` CHAR(36) DEFAULT NULL,
     `modified_by` CHAR(36) DEFAULT NULL,
     `deleted` TINYINT(1) DEFAULT '0',
-    `username` VARCHAR(255) DEFAULT NULL,
-    `userhash` VARCHAR(255) DEFAULT NULL,
+    `username` VARCHAR(255) DEFAULT NOT NULL,
+    `userhash` VARCHAR(255) DEFAULT NOT NULL,
     `first_name` VARCHAR(255) DEFAULT NULL,
     `last_name` VARCHAR(255) DEFAULT NULL,
-    `email_address` VARCHAR(150) DEFAULT NULL,
+    `email_address` VARCHAR(50) DEFAULT NOT NULL,
     `mobile_no` BIGINT(12),
-    `login_by` TINYINT(5) DEFAULT '0' COMMENT '0=>Email, 1=>Gmail, 2=>Facebook',
-    `status` CHAR(10) DEFAULT NULL,
+    `login_mode` TINYINT(5) DEFAULT '0' COMMENT '0=>Email, 1=>Gmail, 2=>Facebook',
+    `login_from_where` CHAR(15) DEFAULT 'Web' COMMENT 'Web, Mobile',
+    `last_login` DATETIME DEFAULT NULL,
+    `status` CHAR(10) DEFAULT 'Active' COMMENT 'Active, Inactive',
     PRIMARY KEY(id)
 );
