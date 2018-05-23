f1d2e9e1f17b97798ee8b3cdac195c694eaf3d82
aroramanish63 <aroramanish63@gmail.com>
2018-05-23 22:33:34 +0530

    Theme Part component
    Admin panel theme template component added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PagecontentComponent } from './pagecontent/pagecontent.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent,
+    PagecontentComponent
   ],
   imports: [
     BrowserModule
