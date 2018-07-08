8eb764a6554fdabec848070fa9fc38f66dfb06de
aroramanish63 <aroramanish63@gmail.com>
2018-07-08 13:26:37 +0530

    auth guard added for login
    Auth guard file added for login

---
+++ src/app/_guards/auth.guard.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+
+@Injectable()
+export class AuthGuard implements CanActivate{
+
+    public constructor(private router:Router){}
+
+    canActivate(route:ActivatedRouteSnapshot, state:RouterStateSnapshot){
+        if(localStorage.getItem('currentUser')){
+            return true;
+        }
+
+        this.router.navigate(['/login'], {queryParams: {returnUrl: state.url}});
+        return false;
+    }
+}
